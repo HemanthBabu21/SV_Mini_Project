21b980bdb389c2845b11da279e32183be5eaaf20
HemanthBabu21 <169127312+HemanthBabu21@users.noreply.github.com>
2025-03-22 21:14:56 +0530

    Create count_package.sv

---
+++ UP_DOWN_COUNTER/test/count_package.sv
@@ -0,0 +1,12 @@
+package count_pkg; 
+int no_of_transcations=1; 
+`include "count_trans.sv" 
+`include "count_gen.sv" 
+`include "count_write.sv" 
+`include "count_wr_mon.sv" 
+`include "count_rd_mon.sv" 
+`include "count_ref_model.sv" 
+`include "count_sb.sv" 
+`include "count_env.sv" 
+`include "test.sv" 
+endpackage
